3d02c45d05ac2b048bb7fe28d26210c6ceb25f6d
andreyferraz <andrey.developer@hotmail.com>
2026-04-08 09:19:09 -0300

    add produto table schema and model class

---
+++ src/main/java/com/countryburguer/cardapio/model/Produto.java
@@ -0,0 +1,28 @@
+package com.countryburguer.cardapio.model;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("produto")
+public class Produto {
+
+    @Id
+    private UUID id;
+    private UUID categoriaId;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private String imagemUrl;
+    private Integer ativo;
+
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,14 @@ CREATE TABLE IF NOT EXISTS categoria (
     nome VARCHAR(255) NOT NULL,
     ordem_exibicao INTEGER NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS produto (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    preco DECIMAL(10, 2) NOT NULL,
+    imagem_url VARCHAR(255),
+    ativo INTEGER NOT NULL DEFAULT 1 CHECK (ativo IN (0, 1)),
+    categoria_id INTEGER,
+    FOREIGN KEY (categoria_id) REFERENCES categoria(id)
+);
